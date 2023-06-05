2eb82e9d46f7d6512e4d7923100374a6da1a529a
LibbertDR <albertodironza84@gmail.com>
2023-06-05 10:19:24 +0200

    DragAndDrop
    Added drag constraint to the ingredients gameObject

--- HopNBrew/Assets/Scripts/DragAndDrop.cs
+++ HopNBrew/Assets/Scripts/DragAndDrop.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Drag and Drop management
+/// </summary>
 public class DragAndDrop : MonoBehaviour
 {
     Vector3 mousePositionOffset;
     Vector3 startingPosition;
     private Vector3 GetMouseWorldPosition()
     {
-        // capture mouse position & return WorldPoint
+        //FIXME capture mouse position & return WorldPoint
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
     //OnMouseDown is called on the first frame of mouse click
@@ -21,11 +24,19 @@ public class DragAndDrop : MonoBehaviour
     //OnMouseDrag is called on all the other frames of mouse click
     private void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPosition() + mousePositionOffset;
+        
+            transform.position = GetMouseWorldPosition() + mousePositionOffset;
+        
     }
     //OnMouseUp is called on the last frame of mouse click
     private void OnMouseUp()
     {
         transform.position = startingPosition;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.0f, 2.0f), Mathf.Clamp(transform.position.y, -5.0f, 0.1f), 0);
+
     }
 }
