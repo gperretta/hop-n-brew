a324751db5d43c876d87201ffca4296059136637
gperretta <113616815+gperretta@users.noreply.github.com>
2023-06-21 10:45:40 +0200

    Deactivate text during tutorial.

--- HopNBrew/Assets/Scripts/StartGame.cs
+++ HopNBrew/Assets/Scripts/StartGame.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
     public GameObject tutorialCanvas;
     public bool canStart;
+    public GameObject counterObject;
 
     private void Awake()
     {
@@ -15,6 +17,7 @@ public class StartGame : MonoBehaviour
     public void exitTutorial()
     {
         tutorialCanvas.SetActive(false);
+        counterObject.SetActive(true);
         canStart = true;
     }
 }
